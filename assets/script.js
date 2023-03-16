7db489c0ed6216293ed62a212c8682e1f69373b5
Joel Perez <jxp489@gmail.com>
2023-03-16 11:41:41 -0700

    added js logic

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,25 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+for(var y = 0; y < topics.length; y++){
+  console.log(topics[y]);
+}
+
+/*
 if (topic === 'HTML') {
-  console.log("Let's study HTML!");
+ console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
+ console.log("Let's study CSS!");
 } else if (topic === 'Git') {
-  console.log("Let's study Git!");
+ console.log("Let's study Git!");
 } else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
+ console.log("Let's study JavaScript!");
 } else {
-  console.log('Please try again!');
+ console.log('Please try again!');
+}
+*/
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
 }
